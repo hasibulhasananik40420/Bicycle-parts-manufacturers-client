c9ac313ded38cc9166564eaaa4d5f2fa9a116a52
hasibulhasananik40420 <hasibulhasananik40420@gmail.com>
2022-05-22 14:24:41 +0600

    add a bautifull section

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import BussinessSummary from './BussinessSummary';
 import Contact from './Contact';
 import Header from './Header';
+import OurTeams from './OurTeams';
 
 const Home = () => {
     return (
         <div>
           <Header></Header>
            <BussinessSummary></BussinessSummary>
+            <OurTeams></OurTeams>
            <Contact></Contact>
         </div>
     );
